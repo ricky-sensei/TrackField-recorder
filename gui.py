07edd6a563a96952c558ee3ee78e2e6d620da5b1
axcelcamp ricky <axcelcamp.teacher@gmail.com>
2024-09-27 16:14:41 +0900

    guiだいたいできた　あとは実行時のファイル初期化と、リファクタリング

--- gui.py
+++ gui.py
@@ -1,17 +1,69 @@
 import tkinter as tk
+import subprocess
+import os
+from main import main
+
+school_list = [
+    "大更小学校",
+    "平舘小学校",
+    "田頭小学校",
+    "柏台小学校",
+    "松野小学校",
+    "寄木小学校",
+    "平笠小学校",
+    "安代小学校",
+    "寺田小学校",
+    "田山小学校",
+]
+
 
 
 root = tk.Tk()
-
 root.geometry("400x400")
 
-button_setResults = tk.Button(root, text="リザルトファイルを追加", width= 200)
-button_setEntries = tk.Button(root, text="入力用ファイルを追加")
-button_execute = tk.Button(root, text="入力用ファイルんい書き込む")
 
-button_setResults.pack(anchor="center", expand=True)
-button_setEntries.pack(anchor="center", expand=True)
+variable = tk.StringVar(root)
+variable.set(school_list[0])
+selected_school = ""
+
+def open_record_folder():
+    path = os.path.abspath("./data/record")
+    checkFolder(path)
+
+def open_input_folder():
+    path = os.path.abspath("./data/input")
+    checkFolder(path)
+
+def checkFolder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    subprocess.Popen(['explorer', path], shell=True)
+
+def execute():
+    print(selected_school)
+    if selected_school in school_list:
+        print("a")
+        main(selected_school[:-3])
+
+def callback(*args):
+    global selected_school
+    selected_school = variable.get()
+    print(selected_school)
+
+button_setRecords = tk.Button(root, text="リザルトファイルを追加",command=open_record_folder)
+button_setInputs = tk.Button(root, text="入力用ファイルを追加",command=open_input_folder)
+button_execute = tk.Button(root, text="入力用ファイルに書き込む", command=execute)
+opt = tk.OptionMenu(root, variable, *school_list)
+opt.config(width=90)
+
+
+
+
+button_setRecords.pack(anchor="center", expand=True)
+button_setInputs.pack(anchor="center", expand=True)
 button_execute.pack(anchor="center", expand=True)
+opt.pack(anchor="center", expand=True)
+variable.trace("w", callback)
 
 root.mainloop()
 
--- main.py
+++ main.py
@@ -3,26 +3,29 @@ import pprint
 from getrecord import GetRecord
 from input import data_input
 
-TrackField_list = [
+def main(selected_school):
+    TrackField_list = [
     glob.glob("./data/record/５年女子/*"),
     glob.glob("./data/record/５年男子/*"),
     glob.glob("./data/record/６年女子/*"),
     glob.glob("./data/record/６年男子/*"),
-]
-relay_list = glob.glob("./data/record/リレー/*")
+    ]
+    relay_list = glob.glob("./data/record/リレー/*")
 
-TrackField_record = GetRecord()
-Relay_record = GetRecord()
+    TrackField_record = GetRecord()
+    Relay_record = GetRecord()
 
-selected_school = input("学校名：")
 
-for event in TrackField_list:
-    TrackField_record.get_row_number(event, selected_school)
+    for event in TrackField_list:
+        TrackField_record.get_row_number(event, selected_school)
 
-Relay_record.get_row_number(relay_list, selected_school)
+    Relay_record.get_row_number(relay_list, selected_school)
 
-pprint.pprint(TrackField_record.school_record)
-pprint.pprint(Relay_record.school_record)
+    pprint.pprint(TrackField_record.school_record)
+    pprint.pprint(Relay_record.school_record)
 
-data_input(TrackField_record.school_record, "data/input/")
-data_input(Relay_record.school_record, "data/input/Relay_result.xlsx")
+    data_input(TrackField_record.school_record, "data/input/TrackField_result.xlsx")
+    data_input(Relay_record.school_record, "data/input/Relay_result.xlsx")
+
+
+
